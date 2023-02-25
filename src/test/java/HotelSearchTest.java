9b8aa8e133dfb079667dc351d438fd9f5dc9b906
Jakub Kubala <j.kubala92@gmail.com>
2023-02-25 13:19:25 +0100

    Add 1 adult and 1 child

--- src/test/java/HotelSearchTest.java
+++ src/test/java/HotelSearchTest.java
@@ -16,13 +16,18 @@ public class HotelSearchTest {
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
 
-        // searching for & setting the city
+        // setting the city
         driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
 
-        // searching for and setting the date
+        // setting check in and check out dates
         driver.findElement(By.name("checkin")).sendKeys("17/04/2023");
         driver.findElement(By.name("checkout")).sendKeys("20/04/2023");
+
+        // adding 1 adult and 1 child
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
     }
 }
