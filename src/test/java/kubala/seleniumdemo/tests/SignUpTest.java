30539c571378b0103ce5e75e8745a6e25939b5d4
Jakub Kubala <j.kubala92@gmail.com>
2023-02-25 21:36:43 +0100

    Add SignUpPage Add openSignUpForm() Refactor first test in SignUpTest

--- src/test/java/kubala/seleniumdemo/tests/SignUpTest.java
+++ src/test/java/kubala/seleniumdemo/tests/SignUpTest.java
@@ -1,5 +1,7 @@
 package kubala.seleniumdemo.tests;
 
+import kubala.seleniumdemo.pages.HotelSearchPage;
+import kubala.seleniumdemo.pages.SignUpPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -12,23 +14,23 @@ public class SignUpTest extends BaseTest {
     @Test
     public void signUpTest() {
 
-        // opening the sign-up form
-        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
-        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
-
         // add firstName, lastName, phoneNumber, unique/random emailAddress to the sign-up form
-        String lastName = "Kubala";
+        String lastName = "Kijania";
         int randomNumber = (int) (Math.random()*1000);
-        String email = "testJakub" + randomNumber + "@testing.pl";
-        driver.findElement(By.name("firstname")).sendKeys("Jakub");
-        driver.findElement(By.name("lastname")).sendKeys(lastName);
-        driver.findElement(By.name("phone")).sendKeys("111111111");
-        driver.findElement(By.name("email")).sendKeys(email);
+        String email = "testPiotr" + randomNumber + "@testing.pl";
 
-        // add password to the sign-up form, click sign up and check if we get to the signed user page
-        driver.findElement(By.name("password")).sendKeys("Test123");
-        driver.findElement(By.name("confirmpassword")).sendKeys("Test123");
-        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+        hotelSearchPage.openSignUpForm();
+
+        SignUpPage signUpPage = new SignUpPage(driver);
+        signUpPage.setFirstName("Piotr");
+        signUpPage.setLastName(lastName);
+        signUpPage.setPhone("123456789");
+        signUpPage.setEmail(email);
+        signUpPage.setPassword("Haslo123");
+        signUpPage.confirmPassword("Haslo123");
+        signUpPage.signUp();
+
         WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
         Assert.assertTrue(heading.getText().contains(lastName));
     }
