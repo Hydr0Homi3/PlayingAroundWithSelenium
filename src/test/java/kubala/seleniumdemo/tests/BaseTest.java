6e6c2c15911ba281a194d3e5b1e86d3bc095957a
Jakub Kubala <j.kubala92@gmail.com>
2023-02-26 21:05:26 +0100

    Create Data Provider Add raports Add screenshots to raports

--- src/test/java/kubala/seleniumdemo/tests/BaseTest.java
+++ src/test/java/kubala/seleniumdemo/tests/BaseTest.java
@@ -1,15 +1,34 @@
 package kubala.seleniumdemo.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import kubala.seleniumdemo.utils.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import java.io.IOException;
 
 public class BaseTest {
 
     protected WebDriver driver;
+    protected static ExtentSparkReporter sparkReporter;
+    protected static ExtentReports extentReports;
+
+
+    @BeforeSuite
+    public void beforeSuite() {
+        sparkReporter = new ExtentSparkReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(sparkReporter);
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        extentReports.flush();
+    }
 
     @BeforeMethod
     public void setup() throws IOException {
--- src/test/java/kubala/seleniumdemo/tests/HotelSearchTest.java
+++ src/test/java/kubala/seleniumdemo/tests/HotelSearchTest.java
@@ -1,41 +1,86 @@
 package kubala.seleniumdemo.tests;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import kubala.seleniumdemo.pages.HotelSearchPage;
 import kubala.seleniumdemo.pages.ResultsPage;
+import kubala.seleniumdemo.utils.ExcelReader;
+import kubala.seleniumdemo.utils.SeleniumHelper;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.List;
 
 public class HotelSearchTest extends BaseTest {
     @Test
-    public void searchHotelTest() {
-
+    public void searchHotelTest() throws IOException {
+        ExtentTest test = extentReports.createTest("Search Hotel Test");
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
-        List<String> hotelNames = hotelSearchPage
-                .setCity("Dubai")
-                .setDates("27/04/2023", "29/04/2023")
-                .setTravellers(1, 1)
-                .performSearch().getHotelNames();
+        hotelSearchPage.setCity("Dubai");
+        test.log(Status.PASS,"Setting city done", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.setDates("27/04/2023", "29/04/2023");
+        test.log(Status.PASS,"Setting dates done", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.setTravellers(1, 1);
+        test.log(Status.PASS,"Setting travellers done", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.performSearch().getHotelNames();
+        test.log(Status.PASS,"Performing search done");
+        test.log(Status.PASS,"Screenshot", SeleniumHelper.getScreenshot(driver));
+
+        ResultsPage resultsPage = new ResultsPage(driver);
+        List<String> hotelNames = resultsPage.getHotelNames();
 
         Assert.assertEquals(hotelNames.get(0), "Jumeirah Beach Hotel");
         Assert.assertEquals(hotelNames.get(1), "Oasis Beach Tower");
         Assert.assertEquals(hotelNames.get(2), "Rose Rayhaan Rotana");
         Assert.assertEquals(hotelNames.get(3), "Hyatt Regency Perth");
-
+        test.log(Status.PASS,"Assertions passed", SeleniumHelper.getScreenshot(driver));
     }
 
     @Test
-    public void searchHotelWithNoNameTest() {
-
+    public void searchHotelWithNoNameTest() throws IOException {
+        ExtentTest test = extentReports.createTest("Search Hotel Test Without City");
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         hotelSearchPage.setDates("27/04/2023", "29/04/2023");
+        test.log(Status.PASS,"Setting dates done", SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.setTravellers(0, 1);
+        test.log(Status.PASS,"Setting travellers done", SeleniumHelper.getScreenshot(driver));
         hotelSearchPage.performSearch();
+        test.log(Status.PASS,"Performing search done");
+        test.log(Status.PASS,"Screenshot", SeleniumHelper.getScreenshot(driver));
 
         ResultsPage resultsPage = new ResultsPage(driver);
 
         Assert.assertTrue(resultsPage.resultHeading.isDisplayed());
         Assert.assertEquals(resultsPage.getHeadingText(), "No Results Found");
+        test.log(Status.PASS,"Assertions passed", SeleniumHelper.getScreenshot(driver));
+    }
+
+    @Test(dataProvider = "data")
+    public void searchHotelTestWithDataProvider(String city, String hotelName) throws IOException {
+        ExtentTest test = extentReports.createTest("Search Hotel Test With Data Provider for " + city);
+        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+        hotelSearchPage.setCity(city);
+        test.log(Status.PASS,"Setting city done", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.setDates("27/04/2023", "29/04/2023");
+        test.log(Status.PASS,"Setting dates done", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.setTravellers(1, 1);
+        test.log(Status.PASS,"Setting travellers done", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.performSearch();
+        test.log(Status.PASS,"Performing search done");
+        test.log(Status.PASS,"Screenshot", SeleniumHelper.getScreenshot(driver));
+
+        ResultsPage resultsPage = new ResultsPage(driver);
+        List<String> hotelNames = resultsPage.getHotelNames();
+
+        Assert.assertEquals(hotelNames.get(0), hotelName);
+        test.log(Status.PASS,"Assertions passed", SeleniumHelper.getScreenshot(driver));
+
+    }
+
+    @DataProvider
+    public Object[][] data() throws IOException {
+        return ExcelReader.readExcel("testData.xlsx");
     }
 }
