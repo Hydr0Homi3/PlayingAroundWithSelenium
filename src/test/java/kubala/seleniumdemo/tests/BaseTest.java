87ae6a5193e3b5eb5371978213d02c59b3134021
Jakub Kubala <j.kubala92@gmail.com>
2023-02-26 14:01:33 +0100

    Add DriverFactory Refactor BaseTest

--- src/test/java/kubala/seleniumdemo/tests/BaseTest.java
+++ src/test/java/kubala/seleniumdemo/tests/BaseTest.java
@@ -1,8 +1,7 @@
 package kubala.seleniumdemo.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import kubala.seleniumdemo.utils.DriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,8 +13,7 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = DriverFactory.getDriver("firefox");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
